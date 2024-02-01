2f1022297a0bac4c5006c84893e768332b9611d2
fnxiel <fnxiel@unitec.edu>
2024-01-31 20:43:24 -0600

    Ejercicio-31-01-2023

---
+++ intro-typescript.js
@@ -0,0 +1,39 @@
+//console.log("Hola")
+
+let saludo = "Hola mundo"
+
+console.log(saludo)
+
+saludo = 100
+
+console.log(saludo)
+
+saludo = true
+
+console.log(saludo)
+
+saludo = new Date()
+
+console.log(saludo)
+
+if(saludo === true){
+    console.log("Saludo es un booleano")
+}
+
+saludo = null
+
+console.log(saludo)
+
+saludo = "Hola otra vez"
+
+console.log(saludo)
+
+const persona = {
+    nombre: "Hola",
+    edad: 25
+}
+
+persona.edad = "Hola"
+persona.apellido = "Apellido"
+
+console.log(persona)
